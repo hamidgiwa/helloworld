46a9fbd7bd6b4a1495fd2e391031a5598f517c59
hamidgiwa <61986399+hamidgiwa@users.noreply.github.com>
2020-07-01 03:46:24 -0700

    Add files via upload

---
+++ Assingment 7.py
@@ -0,0 +1,9 @@
+#programm by Hamid
+#program to find the sum of all items in a dictionary
+
+def sum list(dictonary)  :
+    sum_numbers = 0
+    for y in the dictionary:
+        sum_numbers += y
+        returm sum_numbers
+        print(sum_list(1,2,-8))

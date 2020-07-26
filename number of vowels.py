feb3e36680957800b27bbc9f806a3e35507084ca
hamidgiwa <61986399+hamidgiwa@users.noreply.github.com>
2020-07-26 01:22:15 -0700

    Add files via upload

---
+++ number of vowels.py
@@ -0,0 +1,14 @@
+program to countowel in a string using set
+def countvowel(str1):
+    c =0
+    # Creating a set of vowels
+    s = "aeiouAEIOU"
+    v = set(s)
+
+    #Loop to transverse the alphabet in the given string
+for alphabet in str:
+    #If alphabet is present
+    # in the set vowel:
+            count = count + 1
+
+    print("No of vowels:", count)
